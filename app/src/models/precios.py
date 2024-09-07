16bab5017e6a998108dc5004bd2c43019b3f79c6
Leymar <buenaventuraleymar901@gmail.com>
2024-09-07 12:26:38 -0500

    de desarrollo moduolo preiocio

--- app/src/models/precios.py
+++ app/src/models/precios.py
@@ -1,13 +1,42 @@
+import os
+import json
+
+
 class Precios:
-
     def __init__(self):
-        self.precio_chapa = float()
-        self.precio_esquina = float()
-        self.precio_acado = float()
-        self.precio_tipo_vidrio = float()
-
-    def actualizar_precios(self):
-        pass
+        self.ruta_archivo_datos = os.path.join("src", "datos", "ventana.json")
+        self.diccionario_datos = {}
+        self.precios_atributos_nave = {}
+        self.precios_atributos_vidrio = {}
+        self.descuentos = {}
 
     def cargar_precios(self):
-        pass
+        with open(self.ruta_archivo_datos, "r") as file:
+            self.diccionario_datos = json.load(file)
+
+            self.precios_atributos_nave = self.diccionario_datos.get(
+                "nave", {})
+            self.precios_atributos_vidrio = self.diccionario_datos.get(
+                "vidrio", {})
+            self.descuentos = self.diccionario_datos.get("descuento", {})
+        return self.diccionario_datos
+
+    def actualizar_precios(self, precios_actualizados):
+        with open(self.ruta_archivo_datos, 'w') as file:
+            json.dump(precios_actualizados, file, indent=4)
+
+    def mostrar_precios(self):
+        if not self.diccionario_datos:  # Verifica si los datos están cargados
+            self.cargar_precios()
+
+        print("Precios de Naves:")
+        for atributo, precio in self.precios_atributos_nave.items():
+            print(f"{atributo}: {precio}")
+
+        print("\nPrecios de Vidrio:")
+        for tipo, precio in self.precios_atributos_vidrio.items():
+            print(f"{tipo}: {precio}")
+
+        print("\nDescuentos:")
+        for key, value in self.descuentos.items():
+            print(f"{key}: {value}")
